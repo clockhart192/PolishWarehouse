4cf4b7172ec0e00edd0fc37890037374251d9c87
Chris Lockhart <clockhart192@gmail.com>
2017-09-21 07:53:56 -0400

    Account functionality in place.

---
+++ PolishWarehouse/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(PolishWarehouse.Startup))]
+namespace PolishWarehouse
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
